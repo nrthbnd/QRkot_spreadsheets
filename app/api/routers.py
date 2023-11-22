30a960acfd058b5922ed6772f7a708c428008f83
nrthbnd <asoloshenkova@gmail.com>
2023-11-22 14:09:06 +0300

    Add google api router

--- app/api/routers.py
+++ app/api/routers.py
@@ -3,9 +3,10 @@ from fastapi import APIRouter
 from constants import (
     CHARITY_PROJECT_ROUTER_PREFIX, CHARITY_PROJECT_ROUTER_TAG,
     DONATION_ROUTER_PREFIX, DONATION_ROUTER_TAG,
+    GOOGLE_ROUTER_PREFIX, GOOGLE_ROUTER_TAG,
 )
 from app.api.endpoints import (
-    charity_project_router, donation_router, user_router,
+    google_api_router, charity_project_router, donation_router, user_router,
 )
 
 
@@ -22,5 +23,9 @@ main_router.include_router(
     prefix=DONATION_ROUTER_PREFIX,
     tags=[DONATION_ROUTER_TAG],
 )
-
+main_router.include_router(
+    google_api_router,
+    prefix=GOOGLE_ROUTER_PREFIX,
+    tags=[GOOGLE_ROUTER_TAG],
+)
 main_router.include_router(user_router)
