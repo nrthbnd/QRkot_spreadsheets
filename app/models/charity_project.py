e1ec28ccc7cf8b9c86a3b206c5af1f0c462b7a70
nrthbnd <asoloshenkova@gmail.com>
2023-11-24 11:31:56 +0300

    isort

--- app/models/charity_project.py
+++ app/models/charity_project.py
@@ -1,10 +1,14 @@
 from sqlalchemy import Column, String, Text
 
-from constants import PR_NAME_MAX_LEN
 from app.models.custombase import CustomBase
+from constants import PR_NAME_MAX_LEN
 
 
 class CharityProject(CustomBase):
     """Модель благотворительного проекта."""
-    name = Column(String(PR_NAME_MAX_LEN), unique=True, nullable=False)
+    name = Column(
+        String(PR_NAME_MAX_LEN),
+        unique=True,
+        nullable=False,
+    )
     description = Column(Text, nullable=False)
