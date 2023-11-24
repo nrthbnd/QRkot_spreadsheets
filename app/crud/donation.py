c144d5e63a6ea1f1315c744582fc3a7ed5570cfa
nrthbnd <asoloshenkova@gmail.com>
2023-11-24 11:36:01 +0300

    isort

--- app/crud/donation.py
+++ app/crud/donation.py
@@ -1,5 +1,5 @@
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.base import CRUDBase
 from app.models import Donation, User
